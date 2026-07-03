7904f23ba2a3f1d5015828058fa62635209bcbb4
Aditi Wandale <aditiwandale@gmail.com>
2026-07-03 18:17:49 +0530

    Update API URL and add redirects

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'volantis-backend-production.up.railway.app',  // ← Your Railway URL
 });
 
 api.interceptors.request.use((config) => {
